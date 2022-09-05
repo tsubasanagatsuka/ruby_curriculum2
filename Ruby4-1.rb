48c8442b484dfeb86452956a74fa14b046bb280b
tsubasanagatsuka <reach.283.0921@gmail.com>
2022-09-06 00:36:26 +0900

    Ruby4

---
+++ Ruby4-1.rb
@@ -0,0 +1,35 @@
+puts "ようこそ！名前を入力してください"
+name = "タロウ"
+
+puts "秒数を知りたい時間を分単位で入力してください"
+minutes = 5
+seconds = minutes * 60
+
+puts "#{name}さん、#{minutes}分は#{seconds}秒です！"
+
+puts "ようこそ！\n名前を入力してください"
+name = "タロウ"
+
+puts "秒数を知りたい時間を分単位で入力してください"
+minutes = 5
+seconds = minutes * 60
+
+puts "#{name}さん、#{minutes}分は#{seconds}秒です！"
+
+puts "ようこそ！\n名前を入力してください"
+name = gets
+
+puts "秒数を知りたい時間を分単位で入力してください"
+minutes = gets
+seconds = minutes * 60
+
+puts "#{name}さん、#{minutes}分は#{seconds}秒です！"
+
+puts "ようこそ！\n名前を入力してください"
+name = gets.chomp
+
+puts "秒数を知りたい時間を分単位で入力してください"
+minutes = gets.to_i
+seconds = minutes * 60
+
+puts "#{name}さん、#{minutes}分は#{seconds}秒です！"
